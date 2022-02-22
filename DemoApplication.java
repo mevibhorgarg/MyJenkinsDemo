528fe1f646163dbfbfdd2f576ba28ef2bb0f509e
mevibhorgarg <364vibhor@gmail.com>
2022-02-22 15:21:18 +0530

    first

---
+++ DemoApplication.java
@@ -0,0 +1,25 @@
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+		Calculate calculate = new Calculate();
+		Integer res = calculate.add(1, 3);
+		System.out.println(res);
+		res = calculate.sub(100, 30);
+		System.out.println(res);
+	}
+
+}
+
+class Calculate {
+
+	public Integer add(Integer a, Integer b) {
+		return a+b;
+	}
+	
+	public Integer sub(Integer a, Integer b) {
+		return a-b;
+	}
+	
+}
+
